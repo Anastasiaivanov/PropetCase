c2caf2917d9eb36d2078d66097361677cbcd6b26
Anastasiaivanov <anastasiaivanova1986@gmail.com>
2021-02-08 17:01:31 +0100

    add_OperaBrowser

--- src/test/java/com/propet/TestBase.java
+++ src/test/java/com/propet/TestBase.java
@@ -3,6 +3,7 @@ package com.propet;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.opera.OperaDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -18,7 +19,8 @@ public class TestBase {
 
     @BeforeClass
     public void setUp() {
-        wd = new ChromeDriver();
+        //wd = new ChromeDriver();
+        wd = new OperaDriver();
         wd.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         wd.manage().window().maximize();
         wd.navigate().to("http://propets-frontend.herokuapp.com/propets");
